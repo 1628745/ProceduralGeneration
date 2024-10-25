351901666651004f711fbeb83c5a1cc8bb640cb8
Jude Caldwell <2025jcaldwel@tjhsst.edu>
2024-10-25 09:52:45 -0400

    Created NoiseFunction.cs

---
+++ Assets/Scripts/NoiseFunction.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseFunction : MonoBehaviour
+{
+    //Noise function
+    //Perlin for base layer (FBM)
+    //Mountains use Voronoi * Simplex
+    //Add perlin to mountains
+
+    
+}
