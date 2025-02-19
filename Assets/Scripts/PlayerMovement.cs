fc21040a510e0720fe2f767acc6330f7da198ed3
Jude Caldwell <2025jcaldwel@tjhsst.edu>
2025-02-19 10:17:48 -0500

    Player Movement Updates
    Added "Player" mode to allow slower movement, and more realistic interaction with the environment. Previous movement system switched to "spectator"

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using PlasticPipe.PlasticProtocol.Messages;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Create a player movement script that uses WASD to move the player around the scene.
-    //The mouse should be used to rotate the player.
-    //The player should be able to jump with the space bar.
-    //The player has a rigidbody component attached to it.
-
+    public bool spectating = true;
     public float speed = 5f; //45 seems good
     public float speedMult = 1f; //3 seems good
     public float rotationSpeed = 100f; //500 seems good
@@ -18,6 +15,11 @@ public class PlayerMovement : MonoBehaviour
     public Camera cam;
     public MeshGenerator meshGen;
     public int renderDistance = 1;
+    int maxBounces = 5;
+    float skinWidth = .015f;
+    float maxSlopeAngle = 55f;
+    //get layer mask for collision
+    int layerMask = 1 << 8;
 
     private Rigidbody rb;
 
@@ -33,11 +35,14 @@ public class PlayerMovement : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         //get camera attached to player
         cam = GetComponentInChildren<Camera>();
+        if (spectating){
+            speed *= 9;
+            speedMult *= 3;
+        }
     }
 
     void Update()
     {
-        //Check to see if current coords have passed into a new chunk seeing if prevCoords/500 != currentCoords/500
         if (prevCoords != null)
         {
             if (Math.Floor(prevCoords.Item1 / xSize) != Math.Floor(rb.position.x / xSize) || Math.Floor(prevCoords.Item2 / zSize) != Math.Floor(rb.position.z / zSize))
@@ -78,7 +83,78 @@ public class PlayerMovement : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
         cam.transform.Rotate(Vector3.left * mouseY);
 
+        //Only call if player isn't spectating
+        if (!spectating){
+            rb.velocity = CollideAndSlide(rb.velocity, rb.position, 0, false, rb.velocity);
+        }
         prevCoords = new Tuple<float, float>(rb.position.x, rb.position.z);
+    }
+
+
+    private Vector3 ProjectAndScale(Vector3 vec, Vector3 normal)
+    {
+        float mag = vec.magnitude;
+        vec = Vector3.ProjectOnPlane(vec, normal).normalized;
+        vec *= mag;
+        return vec;
+    }
+
+
+    private Vector3 CollideAndSlide(Vector3 vel, Vector3 pos, int depth, bool gravityPass, Vector3 velInit)
+    {
+        if (depth >= maxBounces)
+        {
+            return Vector3.zero;
+        }
+
+        float dist = vel.magnitude + skinWidth;
+        Bounds bounds = GetComponent<Collider>().bounds;
+
+        RaycastHit hit;
+
+        if (Physics.SphereCast(pos, bounds.extents.x, vel.normalized, out hit, dist, layerMask)){
+            Vector3 snapToSurface = vel.normalized * (hit.distance - skinWidth);
+            Vector3 leftover = vel - snapToSurface;
+            float angle = Vector3.Angle(Vector3.up, hit.normal);
+
+            if (snapToSurface.magnitude <= skinWidth)
+            {
+                snapToSurface = Vector3.zero;
+            }
+
+            if(angle <= maxSlopeAngle)
+            {
+                if (gravityPass)
+                {
+                    return snapToSurface;
+                }
+
+                //He moved into separate function
+                leftover = ProjectAndScale(leftover, hit.normal);
+            }
+            else
+            {
+                float scale = 1 - Vector3.Dot(
+                    new Vector3(hit.normal.x, 0, hit.normal.z).normalized,
+                    new Vector3(velInit.x, 0, velInit.z).normalized
+                );
+
+                /*if(isGrounded && !gravityPass)
+                {
+                    leftover = ProjectAndScale(
+                        new Vector3(leftover.x, 0, leftover.z),
+                        new Vector3(hit.normal.x, 0, hit.normal.z)
+                    ).normalized;
+                    leftover *= scale;
+                }
+                else{*/
+                    leftover = ProjectAndScale(leftover, hit.normal) * scale;
+                //}
+            }//Not finished
+            return snapToSurface + CollideAndSlide(leftover, pos + snapToSurface, depth + 1, gravityPass, velInit);
+        }
+
+        return vel;
     }
 
     //coroutine to load in chunks
